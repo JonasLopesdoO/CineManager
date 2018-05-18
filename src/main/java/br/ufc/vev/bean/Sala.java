2ecf88687bf3e8d90f3fa5797cf773a604a00882
Maike Bezerra <maike_ti@hotmail.com>
2018-05-18 10:10:37 -0300

    Sala
    Criando Tipo sala

--- src/main/java/br/ufc/vev/bean/Sala.java
+++ src/main/java/br/ufc/vev/bean/Sala.java
@@ -1,13 +1,20 @@
 package br.ufc.vev.bean;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
 public class Sala {
 	
 	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Integer id;
+	private String nome;
+	private TipoSala tipo;
+	private int capacidade;
+	
 	
 	public Integer getId() {
 		return id;
@@ -18,7 +25,7 @@ public class Sala {
 	}
 	
 	public Sala() {
-		// TODO Auto-generated constructor stub
+		
 	}
 	
 	
